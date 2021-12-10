5e5a3a71b7123c8a409b57d3dcedb0cf4056608e
Zidapapa <isjang98@woowahan.com>
2021-12-10 22:39:20 +0900

    week 2 - week 2-1-Layouts in JetpackCompose

---
+++ week 2-1-Layouts in JetpackCompose/week 2/app/src/main/java/pri/isjang98/layoutinjetpackcompose/MainActivity.kt
@@ -0,0 +1,434 @@
+package pri.isjang98.layoutinjetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.*
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.layout.Layout
+import androidx.compose.ui.layout.layoutId
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
+import coil.compose.rememberImagePainter
+import kotlinx.coroutines.launch
+import pri.isjang98.layoutinjetpackcompose.ui.theme.LayoutInJetpackComposeTheme
+
+val topics = listOf(
+    "Arts & Crafts", "Beauty", "Books", "Business", "Comics", "Culinary",
+    "Design", "Fashion", "Film", "History", "Maths", "Music", "People", "Philosophy",
+    "Religion", "Social sciences", "Technology", "TV", "Writing"
+)
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ScrollingList()
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Composable
+fun PhotographerCard(modifier: Modifier = Modifier) {
+    Row(modifier = modifier
+        .padding(8.dp)
+        .clip(RoundedCornerShape(4.dp))
+        .background(MaterialTheme.colors.surface)
+        .clickable { }
+        .padding(16.dp)) {
+        Surface(
+           modifier = Modifier.size(50.dp),
+           shape = CircleShape,
+           color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {  }
+        Column(modifier = Modifier
+            .padding(start = 8.dp)
+            .align(Alignment.CenterVertically)) {
+            Text(text = "Alfred Sisley", fontWeight = FontWeight.Bold)
+
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text(text = "3 minutes ago", style = MaterialTheme.typography.body2)
+            }
+        }
+    }
+}
+
+@Composable
+fun LayoutsCodelab() {
+    Scaffold(topBar = {
+        TopAppBar(title = {
+            Text(text = "LayoutsCodelab")
+        }, actions = {
+            IconButton(onClick = {  }) {
+                Icon(imageVector = Icons.Filled.Favorite, contentDescription = null)
+            }
+        })
+    }) { innerPadding ->
+        BodyContent(Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+fun BodyContent(modifier: Modifier = Modifier) {
+    Row(modifier = modifier
+        .background(color = Color.LightGray, shape = RectangleShape)
+        .padding(16.dp)
+        .size(200.dp)
+        .horizontalScroll(
+            rememberScrollState()
+        )) {
+        StaggeredGrid(modifier = modifier.padding(8.dp)) {
+            for (topic in topics) {
+                Chip(modifier = Modifier.padding(8.dp), text = topic)
+            }
+        }
+    }
+}
+
+@Composable
+fun SimpleList() {
+    val scrollState = rememberScrollState()
+    Column(modifier = Modifier.verticalScroll(scrollState)) {
+        repeat(100) {
+            Text("Item #$it")
+        }
+    }
+}
+
+@Composable
+fun LazyList() {
+    val scrollState = rememberLazyListState()
+    LazyColumn(state = scrollState) {
+        items(100) {
+            ImageListItem(it)
+        }
+    }
+}
+
+@Composable
+fun ScrollingList() {
+    val listSize = 100
+    val scrollState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+
+    Column {
+        Row {
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(0)
+                }
+            }) {
+                Text("top")
+            }
+
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(listSize - 1)
+                }
+            }) {
+                Text("bottom")
+            }
+        }
+        LazyColumn(state = scrollState) {
+            items(listSize) {
+                ImageListItem(index = it)
+            }
+        }
+    }
+}
+
+@Composable
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(painter = rememberImagePainter(
+            data = "https://developer.android.com/images/brand/Android_Robot.png"),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp))
+        Spacer(modifier = Modifier.width(10.dp))
+        Text("Item #$index", style = MaterialTheme.typography.subtitle1)
+    }
+}
+
+@Composable
+fun MyOwnColumn(
+    modifier: Modifier = Modifier,
+    content: @Composable () -> Unit) {
+    Layout(modifier = modifier, content = content) { measurables, constraints ->
+        val placeables = measurables.map { measurable ->
+            measurable.measure(constraints = constraints)
+        }
+
+        var yPosition = 0
+
+        layout(constraints.maxWidth, constraints.maxHeight) {
+            placeables.forEach { placeable ->
+                placeable.placeRelative(x = 0, y = yPosition)
+                yPosition += placeable.height
+            }
+        }
+    }
+}
+
+@Composable
+fun StaggeredGrid(
+    modifier: Modifier = Modifier,
+    rows: Int = 3,
+    content: @Composable () -> Unit) {
+    Layout(
+        modifier = modifier,
+        content = content) { measurables, constraints ->
+        val rowWidths = IntArray(rows) { 0 }
+        val rowHeights = IntArray(rows) { 0 }
+
+        val placeables = measurables.mapIndexed { index, measurable ->
+            val placeable = measurable.measure(constraints = constraints)
+
+            val row = index % rows
+            rowWidths[row] += placeable.width
+            rowHeights[row] = Math.max(rowHeights[row], placeable.height)
+            placeable
+        }
+
+        val width = rowWidths.maxOrNull()?.coerceIn(constraints.minWidth.rangeTo(constraints.maxWidth)) ?: constraints.minWidth
+        val height = rowHeights.sumOf { it }.coerceIn(constraints.minHeight.rangeTo(constraints.maxHeight))
+
+        val rowY = IntArray(rows) { 0 }
+        for (i in 1 until rows) {
+            rowY[i] = rowY[i-1] + rowHeights[i-1]
+        }
+
+        layout(width, height) {
+            val rowX = IntArray(rows) { 0 }
+            placeables.forEachIndexed { index, placeable ->
+                val row = index % rows
+
+                placeable.placeRelative(x = rowX[row], y = rowY[row])
+                rowX[row] += placeable.width
+            }
+        }
+    }
+}
+
+@Composable
+fun Chip(modifier: Modifier = Modifier, text: String) {
+    Card(
+        modifier = modifier,
+        border = BorderStroke(color = Color.Black, width = Dp.Hairline),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(modifier = Modifier.padding(start = 8.dp, top = 4.dp, end = 8.dp, bottom = 4.dp), 
+            verticalAlignment = Alignment.CenterVertically) {
+            Box(modifier = Modifier
+                .size(16.dp, 16.dp)
+                .background(color = MaterialTheme.colors.secondary))
+            Spacer(modifier = Modifier.width(4.dp))
+            Text(text = text)
+        }
+    }
+}
+
+@Composable
+fun ConstraintLayoutContent() {
+    ConstraintLayout {
+        val (button1, button2, text) = createRefs()
+
+        Button(onClick = {  },
+            modifier = Modifier.constrainAs(button1) {
+                top.linkTo(parent.top, margin = 16.dp)
+            }) {
+            Text("Button")
+        }
+
+        Text("Text", Modifier.constrainAs(text) {
+            top.linkTo(button1.bottom, margin = 16.dp)
+            centerHorizontallyTo(parent)
+        })
+
+        val barrier = createEndBarrier(button1, text)
+        Button(onClick = { }, modifier = Modifier.constrainAs(button2){
+            top.linkTo(parent.top, margin = 16.dp)
+            start.linkTo(barrier)
+        }) {
+            Text("Button 2")
+        }
+    }
+}
+
+@Composable
+fun LargeConstraintLayout() {
+    ConstraintLayout {
+        val text = createRef()
+        val guideline = createGuidelineFromStart(fraction = 0.5f)
+        Text("This is a very very very very very very very very long text", modifier = Modifier.constrainAs(text) {
+            linkTo(start = guideline, end = parent.end)
+            width = Dimension.preferredWrapContent
+        })
+    }
+}
+
+@Composable
+fun DecoupledConstraintLayout() {
+    BoxWithConstraints {
+        val constraints = if (maxWidth < maxHeight) {
+            decoupledConstraints(margin = 16.dp)
+        } else {
+            decoupledConstraints(margin = 32.dp)
+        }
+
+        ConstraintLayout(constraints) {
+            Button(onClick = { }, modifier = Modifier.layoutId("button")) {
+                Text("Button")
+            }
+            Text("Text", Modifier.layoutId("text"))
+        }
+    }
+}
+
+private fun decoupledConstraints(margin: Dp): ConstraintSet {
+    return ConstraintSet {
+        val button = createRefFor("button")
+        val text = createRefFor("text")
+
+        constrain(button) {
+            top.linkTo(parent.top, margin = margin)
+        }
+        constrain(text) {
+            top.linkTo(button.bottom, margin = margin)
+        }
+    }
+}
+
+@Composable
+fun TwoTexts(modifier: Modifier = Modifier, text1: String, text2: String) {
+    Row(modifier = modifier.height(IntrinsicSize.Min)) {
+        Text(modifier = Modifier
+            .weight(1f)
+            .padding(start = 4.dp)
+            .wrapContentWidth(Alignment.Start),
+            text = text1)
+
+        Divider(color = Color.Black, modifier = Modifier
+            .fillMaxHeight()
+            .width(1.dp))
+        Text(modifier = Modifier
+            .weight(1f)
+            .padding(end = 4.dp)
+            .wrapContentWidth(Alignment.End),
+            text = text2)
+    }
+}
+
+
+//Preview -------------------------------------------------------------------------
+
+@Preview
+@Composable
+fun TwoTextsPreview() {
+    LayoutInJetpackComposeTheme {
+        Surface {
+            TwoTexts(text1 = "Hi", text2 = "there")
+        }
+    }
+}
+
+@Preview
+@Composable
+fun LargeConstraintLayoutPreview() {
+    LayoutInJetpackComposeTheme {
+        LargeConstraintLayout()
+    }
+}
+
+
+@Preview
+@Composable
+fun ConstraintLayoutContentPreview() {
+    LayoutInJetpackComposeTheme {
+        ConstraintLayoutContent()
+    }
+}
+
+@Preview
+@Composable
+fun ChipPreview() {
+    LayoutInJetpackComposeTheme {
+        Chip(text = "Hi there")
+    }
+}
+
+
+@Preview
+@Composable
+fun TextWithPaddingToBaselinePreview() {
+    LayoutInJetpackComposeTheme {
+        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+@Preview
+@Composable
+fun TextWithNormalPaddingPreview() {
+    LayoutInJetpackComposeTheme {
+        Text("Hi there!", Modifier.firstBaselineToTop(32.dp))
+    }
+}
+
+
+
+@Preview
+@Composable
+fun ListPreview() {
+    LayoutInJetpackComposeTheme {
+        LazyList()
+    }
+}
+
+
+@Preview
+@Composable
+fun LayoutsCodelabPreview() {
+    LayoutInJetpackComposeTheme {
+        LayoutsCodelab()
+    }
+}
+
+@Preview
+@Composable
+fun PhotographerCardPreview() {
+    LayoutInJetpackComposeTheme {
+        PhotographerCard()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    LayoutInJetpackComposeTheme {
+        Greeting("Android")
+    }
+}
